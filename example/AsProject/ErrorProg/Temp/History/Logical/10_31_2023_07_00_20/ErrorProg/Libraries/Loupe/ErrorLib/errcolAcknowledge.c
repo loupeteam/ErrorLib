2d5f015f8a60f1566ce70b58f6b0e7c24f0a8409
jcjakeway <james@loupe.team>
2023-10-31 07:55:01 -0700

    Initial update for error lib LPM package

---
+++ example/AsProject/ErrorProg/Temp/History/Logical/10_31_2023_07_00_20/ErrorProg/Libraries/Loupe/ErrorLib/errcolAcknowledge.c
@@ -0,0 +1,100 @@
+/********************************************************************
+ * COPYRIGHT -- Automation Resources Group
+ ********************************************************************
+ * Library: ErrorLib
+ * File: errcolAcknowledge.c
+ * Author: David Blackburn
+ * Created: December 05, 2011
+ ********************************************************************
+ * Implementation of library ErrorLib
+ ********************************************************************/
+
+#include <bur/plctypes.h>
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	#include "ErrorLib.h"
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#include <string.h>
+
+
+/********************************************************************/
+/* Acknowledge an error in the error list							*/
+/********************************************************************/
+
+
+/* Check for null pointer		DONE and TESTED DFB 20130715 */
+/* Check valid ErrorIndex		DONE and TESTED DFB 20130715 */
+/* Set Acknowledged				DONE and TESTED DFB 20130715 */
+/* Log acknowledgement			DONE and TESTED DFB 20130715 */
+/* Set pAcknowledge				DONE and TESTED DFB 20130715 */
+	
+
+unsigned short errcolAcknowledge(unsigned short ErrorIndex, struct ErrorCollector_typ* t)
+{
+	
+	
+/************************************************/
+/* Check for invalid inputs						*/
+/************************************************/
+
+if( t == 0 ){
+		
+	return (UINT)ERRCOL_ERR_INVALIDINPUT;
+		
+} // Check for null pointer //
+
+
+if( ErrorIndex > (t->Internal.NumErrors - 1) ){
+	
+	//errcolInternalSetError( (UINT)ERRCOL_ERR_INVALIDINPUT, t );
+	
+	return 0;
+	
+} // Check for invalid ErrorIndex //
+
+
+/************************************************/
+/* Acknowledge error							*/
+/************************************************/
+
+/* Set ErrorInfo.Acknowledged */
+
+t->OUT.ErrorInfo[ErrorIndex].Acknowledged=	1;
+
+
+/* Log acknowledgement */
+
+UINT LogStatus;
+LogStatus=	0;
+	
+if( !(t->IN.CFG.DisableLogging) ){
+
+	LogStatus=	errcolInternalLogAcknowledge(	(UDINT)&t->OUT.ErrorInfo[ErrorIndex].SourceName,
+												t->OUT.ErrorInfo[ErrorIndex].ErrorID,
+												(UDINT)&t->OUT.ErrorInfo[ErrorIndex].ErrorString,
+												t );
+
+}
+
+
+/* Acknowledge the error at the source */
+	
+if( t->OUT.ErrorInfo[ErrorIndex].pAcknowledge != 0 ){
+	
+	*((BOOL*)t->OUT.ErrorInfo[ErrorIndex].pAcknowledge)=	1;
+	
+}
+
+
+return LogStatus;
+
+
+} // End Fn //
---
+++ example/AsProject/ErrorProg/Temp/History/Logical/10_31_2023_07_00_20/ErrorProg/Libraries/Loupe/ErrorLib/errcolInternalLogError.c
@@ -0,0 +1,92 @@
+/********************************************************************
+ * COPYRIGHT -- Automation Resources Group
+ ********************************************************************
+ * Library: ErrorLib
+ * File: errcolInternalLogError.c
+ * Author: David Blackburn
+ * Created: December 08, 2011
+ ********************************************************************
+ * Implementation of library ErrorLib
+ ********************************************************************/
+
+#include <bur/plctypes.h>
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	#include "ErrorLib.h"
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#include <string.h>
+
+
+/********************************************************************/
+/* Add an error to the logger										*/
+/********************************************************************/
+
+
+/* Check for null pointer		DONE and TESTED DFB 20130715 */
+
+/* Generate display string		DONE and TESTED DFB 20131219 */
+
+/* Add error to logbook			DONE and TESTED DFB 20131219 */
+	/* Set error if error		DONE and TESTED DFB 20130715 */
+
+
+unsigned short errcolInternalLogError(	unsigned long pSourceName, 
+										unsigned short ErrorID, 
+										unsigned long pErrorString, 
+										struct ErrorCollector_typ* t)
+{
+	
+	
+/************************************************/
+/* Check for invalid inputs						*/
+/************************************************/
+
+if( t == 0 ){
+		
+	return (UINT)ERRCOL_ERR_INVALIDINPUT;
+		
+} // Check for null pointer //
+
+
+/************************************************/
+/* Generate DisplayString						*/
+/************************************************/
+
+STRING DisplayString[ERRCOL_STRLEN_DISPLAYSTRING+1];
+
+errcolIntGenerateDisplayString(	(UDINT)DisplayString, 
+								ERRCOL_STRLEN_DISPLAYSTRING,
+								0, 
+								pSourceName, 
+								ErrorID, 
+								pErrorString );
+
+
+/************************************************/
+/* Add error to logbook							*/
+/************************************************/
+
+UINT Status;
+StrExtArgs_typ Args;
+
+Args.s[0]=	(UDINT)&(DisplayString);
+
+Status=	logError( t->IN.CFG.LoggerName, ErrorID, "%s", (UDINT)&(Args) );
+
+if( Status != 0 ){
+	errcolInternalSetError( Status, t );
+}
+
+
+return Status;
+
+
+} // End Fn //
